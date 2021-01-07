9754d9292481123f4693921be589be38c4492683
mateuszb <mateusz.b@aibusters.com>
2021-01-07 14:49:26 +0100

    -groups generation test

--- FromCoco.py
+++ FromCoco.py
@@ -1,10 +1,11 @@
 import csv
 import os
 
+import numpy as np
+
 from InputData import input_data
-from Obj import Obj
 from SceneDescription import scene_description
-from YOLO.img_det import vbox_engine, draw_boxes
+from YOLO.img_det import vbox_engine
 
 
 def save_desc_to_file(description, file_name):
@@ -13,28 +14,53 @@ def save_desc_to_file(description, file_name):
     w.writerow(description)
 
 
-def load_from_file(bound_boxes, file_name, v_labels):
+def load_from_file(bound_boxes, file_name):
     create_bound_boxes, create_prop, create_rels, create_rules, get_field_size = input_data()
     description = scene_description(bound_boxes, create_prop, create_rels, create_rules, get_field_size)
     print(description)
     save_desc_to_file(description, file_name)
 
 
-input_filename = input("Enter a file name to load bBoxes. Data must be delimited with ',': ")
-input_filename = str(input_filename)
+def save_b_boxes(v_boxes, v_labels, input_filename, image_w, image_h):
+    w = csv.writer(open(input_filename, "w"))
 
-# Prints in the console the variable as requested
-print("The file name you entered is: ", input_filename)
-photo_boxed_filename = input_filename.replace('.jpg', '_boxed.jpg')
-v_boxes, v_labels, v_scores, image_w, image_h = vbox_engine(input_filename)
-draw_boxes(input_filename, photo_boxed_filename, v_boxes, v_labels, v_scores)
+    for index, ref_point in enumerate(v_boxes):
+        box = v_boxes[index]
+        color = list(np.random.random(size=3) * 256)
+        XtopLeft = int(box.XtopLeft * 400 / image_w)
+        YtopLeft = int(box.YtopLeft * 400 / image_h)
+        XbottomRight = int(box.XbottomRight * 400 / image_w)
+        YbottomRight = int(box.YbottomRight * 400 / image_h)
+        w.writerow([v_labels[index], XtopLeft, YtopLeft, XbottomRight, YbottomRight, color])
 
-v_boxes_t = {}
-v_boxes_temp = []
-for i in range(len(v_boxes)):
-    box = v_boxes[i]
-    X_len = abs(box.XbottomRight - box.XtopLeft)
-    Y_len = abs(box.YbottomRight - box.YtopLeft)
-    obj = Obj(v_labels[i], [box.YtopLeft, box.XtopLeft], [Y_len, X_len])
-    v_boxes_t[i] = obj
-load_from_file(v_boxes_t, input_filename, v_labels)
+
+def process_for_grouping():
+    os.chdir(r'test')
+    _, _, filenames = next(os.walk(('grouping_test')))
+    os.chdir(r'grouping_test')
+    for idx, input_filename in enumerate(filenames):
+        v_boxes, v_labels, v_scores, image_w, image_h = vbox_engine(input_filename)
+        new_name = 'grouping_test_{}.csv'.format(idx)
+        input_filename = input_filename.replace('.jpg', new_name)
+        save_b_boxes(v_boxes, v_labels, input_filename, image_w, image_h)
+
+
+process_for_grouping()
+# input_filename = input("Enter a file name to load bBoxes. Data must be delimited with ',': ")
+# input_filename = str(input_filename)
+#
+# # Prints in the console the variable as requested
+# print("The file name you entered is: ", input_filename)
+# photo_boxed_filename = input_filename.replace('.jpg', '_boxed.jpg')
+# v_boxes, v_labels, v_scores, image_w, image_h = vbox_engine(input_filename)
+# draw_boxes(input_filename, photo_boxed_filename, v_boxes, v_labels, v_scores)
+#
+# v_boxes_t = {}
+# v_boxes_temp = []
+# for i in range(len(v_boxes)):
+#     box = v_boxes[i]
+#     X_len = abs(box.XbottomRight - box.XtopLeft)
+#     Y_len = abs(box.YbottomRight - box.YtopLeft)
+#     obj = Obj(v_labels[i], [box.YtopLeft, box.XtopLeft], [Y_len, X_len])
+#     v_boxes_t[i] = obj
+# load_from_file(v_boxes_t, input_filename, v_labels)
--- LoadFromFile.py
+++ LoadFromFile.py
@@ -9,8 +9,6 @@ from Intersection import change_to_width_len_format, group_filter_name
 from Obj import Obj
 from SceneDescription import scene_description
 
-image = np.zeros((get_field_size()[0], get_field_size()[1], 3), np.uint8)
-
 
 def save_desc_to_file(description, file_name):
     file_name = file_name.replace('.csv', '_description.csv')
@@ -19,6 +17,7 @@ def save_desc_to_file(description, file_name):
 
 
 def show_rectangles(bound_boxes):
+    image = np.zeros((get_field_size()[0], get_field_size()[1], 3), np.uint8)
     for _, ref_point in enumerate(bound_boxes):
         color = tuple(eval(ref_point[5]))
         l = (int(ref_point[1]), int(ref_point[2]))
@@ -40,12 +39,53 @@ def show_rectangles(bound_boxes):
             break
 
 
-def load_from_file(bound_boxes, readCSV, file_name):
+def show_rectangles_grouping(v_boxes_new, v_boxes_old):
+    def show_img(image_t, b_boxes):
+        for _, ref_point in enumerate(b_boxes):
+            color = tuple(eval(ref_point[5]))
+            l = (int(ref_point[1]), int(ref_point[2]))
+            r = (int(ref_point[3]), int(ref_point[4]))
+            rec = cv2.rectangle(image_t, l, r, color, 2)
+            x, y = int(ref_point[1]), int(ref_point[2])
+            name = str(ref_point[0])
+            cv2.putText(rec, name, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, color, 2)
+        return image_t
+
+    imageNew = np.zeros((get_field_size()[0], get_field_size()[1], 3), np.uint8)
+    imageNew = show_img(imageNew, v_boxes_new)
+    imageOld = np.zeros((get_field_size()[0], get_field_size()[1], 3), np.uint8)
+    imageOld = show_img(imageOld, v_boxes_old)
+    while True:
+        # display the image and wait for a keypress
+        cv2.imshow("imageNEW", imageNew)
+        cv2.imshow("imageOLD", imageOld)
+        key = cv2.waitKey(1) & 0xFF
+        # press 'r' to reset the window
+        # if the 'c' key is pressed, break from the loop
+        cv2.imshow("imageNEW", imageNew)
+        cv2.imshow("imageOLD", imageOld)
+        cv2.imwrite(file_name.replace('.csv', '_image.jpg'), imageNew)
+        cv2.imwrite(file_name.replace('.csv', '_image.jpg'), imageOld)
+        if key == ord("c"):
+            break
+
+
+def load_from_file(bound_boxes, v_boxes_new, file_name, v_boxes_old):
     create_bound_boxes, create_prop, create_rels, create_rules, get_field_size = input_data()
     description = scene_description(bound_boxes, create_prop, create_rels, create_rules, get_field_size)
     print(*description, sep='\n')
     save_desc_to_file(description, file_name)
-    show_rectangles(readCSV)
+    show_rectangles_grouping(v_boxes_new, v_boxes_old)
+
+
+def prepare(v_boxes_n):
+    v_boxes = []
+    for b_box in v_boxes_n:
+        name = b_box[0]
+        XtopLeft, YtopLeft, X_len, Y_len = change_to_width_len_format(b_box)
+        obj = Obj(name, [YtopLeft, XtopLeft], [Y_len, X_len])
+        v_boxes.append(obj)
+    return v_boxes
 
 
 # file_name = input("Enter a file name to load bBoxes. Data must be delimited with ',': ")
@@ -54,8 +94,8 @@ def load_from_file(bound_boxes, readCSV, file_name):
 # # Prints in the console the variable as requested
 # print("The file name you entered is: ", file_name)
 os.chdir(r'test')
-file_name = "interTest2.csv"
-v_boxes = []
+os.chdir(r'grouping_test')
+file_name = "COCO_train2014_000000360452grouping_test_10.csv"
 v_boxes_temp = []
 names_set = set()
 with open(file_name) as csvfile:
@@ -66,11 +106,6 @@ with open(file_name) as csvfile:
             name = box[0]
             names_set.add(name)
 v_boxes_new = group_filter_name(names_set, v_boxes_temp)
-v_boxes_temp = v_boxes_new
-for b_box in v_boxes_temp:
-    name = b_box[0]
-    XtopLeft, YtopLeft, X_len, Y_len = change_to_width_len_format(b_box)
-    obj = Obj(name, [YtopLeft, XtopLeft], [Y_len, X_len])
-    v_boxes.append(obj)
+v_boxes = prepare(v_boxes_new)
 
-load_from_file(v_boxes, v_boxes_temp, file_name)
+load_from_file(v_boxes, v_boxes_new, file_name, v_boxes_temp)
